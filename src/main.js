10f291b3847e73070d7494b4362dd7880eebc0a1
ImagesHunkAdmin <ceo@nethunk.com>
2019-10-27 23:52:54 +0530

    added ssr

--- src/main.js
+++ src/main.js
@@ -2,9 +2,37 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { createRouter } from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export async function createApp ({
+          beforeApp = () => {},
+          afterApp = () => {}
+        } = {}) {
+          const router = createRouter()
+          
+          
+
+          await beforeApp({
+            router,
+            
+            
+          })
+
+          const app = new Vue({
+  router,
+  render: h => h(App)
+})
+
+          const result = {
+            app,
+            router,
+            
+            
+          }
+
+          await afterApp(result)
+
+          return result
+        }
