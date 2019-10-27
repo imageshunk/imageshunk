97dcc47cedc24f67e9ae6cd1968f3587f2fd453f
ImagesHunkAdmin <ceo@nethunk.com>
2019-10-28 00:24:37 +0530

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/"><strong>Images Hunk</strong></router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="#">Collections</a></li>
+          <li>
+            <form class="my-2 m-auto my-lg-0">
+              <input class="form-control m-auto mr-sm-2 search" type="search" placeholder="Search" aria-label="Search">
+            </form>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><router-link class="nav-link" to="#">Login</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="#">Singup</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="#">Cart</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="#">My Account</router-link></li>
+        </ul>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  /*  */
 </style>
