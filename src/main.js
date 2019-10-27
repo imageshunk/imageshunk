56cb4689dc14f1704eabdb0d5653baa020ae11ef
ImagesHunkAdmin <ceo@nethunk.com>
2019-10-28 00:02:12 +0530

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,38 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { createRouter } from './router'
 
 Vue.config.productionTip = false
 
 export async function createApp ({
-          beforeApp = () => {},
-          afterApp = () => {}
-        } = {}) {
-          const router = createRouter()
-          
-          
-
-          await beforeApp({
-            router,
-            
-            
-          })
-
-          const app = new Vue({
-  router,
-  render: h => h(App)
-})
-
-          const result = {
-            app,
-            router,
-            
-            
-          }
-
-          await afterApp(result)
-
-          return result
-        }
+  beforeApp = () => {},
+  afterApp = () => {}
+} = {}) {
+  const router = createRouter()
+  await beforeApp({
+    router,
+  })
+  const app = new Vue({
+    router,
+    render: h => h(App)
+  })
+  const result = {
+    app,
+    router,
+  }
+  await afterApp(result)
+  return result
+}
